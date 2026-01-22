2c19f6fde1d79f5cf62ae5c75a0c90414a43e593
ahtius13 <ahtius13@gmail.com>
2026-01-22 12:57:10 +0100

    Add files via upload

---
+++ test_kata_persistencia.py
@@ -0,0 +1,20 @@
+import json
+import pytest
+
+#Creación de archivo de texto con la palabra "Test" para probar
+
+#Test lectura
+def test_read(file_path):
+#debe leer el archivo y devolver la palabra "Test" para el ok
+    pass
+
+#Test escritura
+def test_write(file_path, content):
+#debe escribir una palabra y comprobar leyendo que estan ambas en el documento
+    pass
+
+#Test actualización
+
+#Test Clear
+
+#Test Delete
